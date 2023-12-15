e09d14bf472e3ae02062b1465969de783de24213
interview-mac <3129571932@qq.com>
2023-12-15 17:18:04 +0800

    加入nacos注册中心

--- user-service/src/main/java/com/hxc/interview/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/hxc/interview/userservice/UserServiceApplication.java
@@ -3,8 +3,10 @@ package com.hxc.interview.userservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.hxc.interview.userservice", "com.hxc.interView"})
 public class UserServiceApplication {
